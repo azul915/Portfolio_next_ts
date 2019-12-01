25752063d3ab8cf55d333c240fb51f9d9e44e447
azul915 <k.aoyagi940516@gmail.com>
2019-12-02 03:52:28 +0900

    インデントを整形

--- src/app/pages/index.tsx
+++ src/app/pages/index.tsx
@@ -11,31 +11,31 @@ import Products from '../components/Products'
 import Blog from '../components/Blog'
 
 const IndexPage: NextPage = () => {
-  return (
-    <Layout title="Kosei Aoyagi">
-      <Grid container>
-        <Grid item xs={12}>
-          <MainImg/>
-        </Grid>
+    return (
+        <Layout title="">
+            <Grid container>
+                <Grid item xs={12}>
+                    <MainImg/>
+                </Grid>
 
-        <Grid item xs={12}>
-          <Profile/>
-        </Grid>
+                <Grid item xs={12}>
+                    <Profile/>
+                </Grid>
 
-        <Grid item xs={12}>
-          <Skills/>
-        </Grid>
+                <Grid item xs={12}>
+                    <Skills/>
+                </Grid>
 
-        <Grid item xs={12}>
-          <Products/>
-        </Grid>
+                <Grid item xs={12}>
+                    <Products/>
+                </Grid>
 
-        <Grid item xs={12}>
-          <Blog/>
-        </Grid>
-      </Grid>
-    </Layout>
-  )
+                <Grid item xs={12}>
+                    <Blog/>
+                </Grid>
+            </Grid>
+        </Layout>
+    )
 }
 
 export default IndexPage
