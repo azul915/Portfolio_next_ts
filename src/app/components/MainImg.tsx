dcae3584d95a52e8821ecdd0a9ddcdb92cd55219
azul915 <k.aoyagi940516@gmail.com>
2019-12-17 01:36:43 +0900

    Add TypingAnimation

--- src/app/components/MainImg.tsx
+++ src/app/components/MainImg.tsx
@@ -1,7 +1,40 @@
 import * as React from 'react'
+import { useState } from 'react'
+import { Typist } from 'react-typist'
+import { makeStyles } from '@material-ui/core/styles'
 
-const MainImg: React.FC = () => (
-    <h1>this is mainimg</h1>
-)
+const useStyles = makeStyles({
+    root: {
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        border: 0,
+        borderRadius: 3,
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+        color: 'white',
+        height: 48,
+        padding: '0 30px',
+    },
+})
 
+const MainImg: React.FC = () => {
+
+    const classes = useStyles();
+    const [typing, setState] = useState({ typing: false });
+    const typingDone = () => {
+        setState({ typing: true });
+    };
+    
+    return (
+        {this:typing
+            ?   <Typist
+                    startDelay={400}
+                    onTypingDone={typingDone}
+                    useStyles={classes.root}
+                >
+                    <h1>Hi! I'm Kosei Aoyagi</h1>
+                </Typist>
+            : ''
+        }
+    );
+
+}
 export default MainImg
---
+++ src/app/typings/react-typist/index.d.ts
@@ -0,0 +1,6 @@
+declare module 'react-typist' {
+    interface TypistProps {
+
+    }
+    export const Typist: React.ComponentType<Typist>
+}
