d6fe917751a5955cbc263fd94f968a31305e2bcb
azul915 <k.aoyagi940516@gmail.com>
2020-01-01 22:03:34 +0900

    仮データで動作確認済

--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -5,27 +5,88 @@ const Skills: React.FC = () => {
     const list = [];
     const skill = [
         {
-            name: 'Java',
-            duration: 25,
-            category: { 
-                id: 0, name: '言語' 
+            category:{
+                id:0,
+                name:"言語"
             },
-            term: 'serverside',
-            self_evaluation : 4,
-            detail: '業務レベルで複雑な実装ができる',
-            created_at: new Date('2020-01-01 00:00:00')
+            created_at:"0001-01-01T00:00:00Z",
+            detail:"SpringBootでAPIコンテナを実装したことがある。",
+            duration:3,
+            name:"Kotlin",
+            self_evaluation:0,
+            term:"serverside"
         },
-
         {
-            name: 'Python',
-            duration: 12,
-            category: {
-                id: 0, name: '言語'
+            category:{
+                id:0,
+                name:"言語"
             },
-            term: 'serverside',
-            self_evaluation : 2,
-            detail: '自動テスト用のSeleniumを書いたことがある',
-            created_at: new Date('2020-01-01 17:00:00')
+            created_at:"2019-06-23T03:34:47.07Z",
+            detail:"Java8で実装ができる",
+            duration:18,
+            name:"Java",
+            self_evaluation:3,
+            term:"serverside"
+        },
+        {
+            category:{
+                id:0,
+                name:"言語"
+            },
+            created_at:"2019-12-07T16:56:23.804817Z",
+            detail:"SpringBootでAPIコンテナを実装したことがある。",
+            duration:3,
+            name:"Kotlin",
+            self_evaluation:4,
+            term:"serverside"
+        },
+        {
+            category:{
+                id:0,
+                name:"言語"
+            },
+            created_at:"2019-12-31T15:00:00Z",
+            detail:"Seleniumでテストを書いたことがある",
+            duration:3,
+            name:"Python",
+            self_evaluation:2,
+            term:"serverside"
+        },
+        {
+            category:{
+                id:0,
+                name:"言語"
+            },
+            created_at:"2019-06-23T06:52:28.755Z",
+            detail:"基本的な文法は理解して、実装することができる。",
+            duration:3,
+            name:"Ruby",
+            self_evaluation:2,
+            term:"serverside"
+        },
+        {
+            category:{
+                id:2,
+                name:"フレームワーク"
+            },
+            created_at:"2019-06-23T06:56:22.93Z",
+            detail:"基本的なCRUDの実装経験がある。",
+            duration:6,
+            name:"Spring",
+            self_evaluation:2,
+            term:"serverside"
+        },
+        {
+            category:{
+                id:2,
+                name:"フレームワーク"
+            },
+            created_at:"2019-06-23T06:27:35.065Z",
+            detail:"基本的なCRUDを作成することができる。",
+            duration:3,
+            name:"rubyonRails",
+            self_evaluation:2,
+            term:"serverside"
         }
     ];
 
