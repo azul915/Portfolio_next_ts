41bd0589f6b1eb2c9f4e7b64b91b94a5657eccd2
azul915 <k.aoyagi940516@gmail.com>
2020-01-05 04:04:32 +0900

    Fix a provisional skills area with Material UI

--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -1,22 +1,39 @@
 import * as React from 'react';
-//import { makeStyles } from '@material-ui/core/styles';
-//import { Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { Grid, Paper, Typography } from '@material-ui/core';
 
-// const useStyles = makeStyles({
-//     card: {
-//         display: 'flex',
-//     },
-//     cardDetails: {
-//         flex: 1,
-//     },
-//     cardMedia: {
-//         width: 160,
-//     },
-// });
+const useStyles = makeStyles(theme => ({
+    mainFeaturedPost: {
+        position: 'relative',
+        backgroundColor: theme.palette.grey[800],
+        color: theme.palette.common.white,
+        marginBottom: theme.spacing(4),
+        backgroundImage: 'url(https://source.unsplash.com/random)',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+    },
+    overlay: {
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        right: 0,
+        left: 0,
+        backgroundColor: 'rgba(0,0,0,.3)',
+    },
+    mainFeaturedPostContent: {
+        position: 'relative',
+        padding: theme.spacing(3),
+        [theme.breakpoints.up('md')]: {
+            padding: theme.spacing(6),
+            paddingRight: 0,
+        },
+    },
+}));
 
 const Skills: React.FC = () => {
 
-    // const classes = useStyles();
+    const classes = useStyles();
 
     const skills = [
         {
@@ -107,21 +124,31 @@ const Skills: React.FC = () => {
 
     const list = skills.map((item) => {
         return (
-            <li>
-                <div>{item.name}</div>
-                <div>{item.duration}</div>
-                <div>{item.category.name}</div>
-                <div>{item.term}</div>
-                <div>{item.self_evaluation}</div>
-                <div>{item.detail}</div>
-            </li>
+            <Paper className={classes.mainFeaturedPost}>
+                <div className={classes.overlay}/>
+                <Grid container>
+                    <Grid item md={6}>
+                        <div className={classes.mainFeaturedPostContent}>
+                            <Typography component="h1" variant="h3" color="inherit" gutterBottom>
+                                {item.name}
+                            </Typography>
+                            <Typography variant="h5" color="inherit" paragraph>
+                                {item.category.name}
+                            </Typography>
+                            <Typography variant="h5" color="inherit" paragraph>
+                                {item.detail}
+                            </Typography>
+                        </div>
+                    </Grid>
+                </Grid>
+            </Paper>
         );
     })
 
     return (
-        <ul>
+        <Grid container spacing={5} >
             {list}
-        </ul>
+        </Grid>
     );
 
 }
