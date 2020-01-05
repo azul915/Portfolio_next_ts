25efe8feedb60325ca1b97f129cba4240c9f412c
azul915 <k.aoyagi940516@gmail.com>
2020-01-06 00:18:01 +0900

    Add provisional axios data rendering

--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -1,6 +1,20 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Grid, Typography, CardActionArea, Card, CardContent, CardMedia, Hidden } from '@material-ui/core';
+import axios from 'axios';
+
+interface Skill {
+    category: {
+        id: number;
+        name: string;
+    };
+    created_at: string;
+    detail: string;
+    duration: number;
+    name: string;
+    self_evaluation: number;
+    term: string;
+}
 
 const useStyles = makeStyles({
     card: {
@@ -14,98 +28,25 @@ const useStyles = makeStyles({
     },
 });
 
-const Skills: React.FC = () => {
+ const Skills: React.FC = () => {
 
     const classes = useStyles();
+    const skills: Skill[] = [];
+    const [data, setData] = useState({ skills });
 
-    const skills = [
-        {
-            category:{
-                id:0,
-                name:"言語"
-            },
-            created_at:"0001-01-01T00:00:00Z",
-            detail:"SpringBootでAPIコンテナを実装したことがある。",
-            duration:3,
-            name:"Kotlin",
-            self_evaluation:0,
-            term:"serverside"
-        },
-        {
-            category:{
-                id:0,
-                name:"言語"
-            },
-            created_at:"2019-06-23T03:34:47.07Z",
-            detail:"Java8で実装ができる",
-            duration:18,
-            name:"Java",
-            self_evaluation:3,
-            term:"serverside"
-        },
-        {
-            category:{
-                id:0,
-                name:"言語"
-            },
-            created_at:"2019-12-07T16:56:23.804817Z",
-            detail:"SpringBootでAPIコンテナを実装したことがある。",
-            duration:3,
-            name:"Kotlin",
-            self_evaluation:4,
-            term:"serverside"
-        },
-        {
-            category:{
-                id:0,
-                name:"言語"
-            },
-            created_at:"2019-12-31T15:00:00Z",
-            detail:"Seleniumでテストを書いたことがある",
-            duration:3,
-            name:"Python",
-            self_evaluation:2,
-            term:"serverside"
-        },
-        {
-            category:{
-                id:0,
-                name:"言語"
-            },
-            created_at:"2019-06-23T06:52:28.755Z",
-            detail:"基本的な文法は理解して、実装することができる。",
-            duration:3,
-            name:"Ruby",
-            self_evaluation:2,
-            term:"serverside"
-        },
-        {
-            category:{
-                id:2,
-                name:"フレームワーク"
-            },
-            created_at:"2019-06-23T06:56:22.93Z",
-            detail:"基本的なCRUDの実装経験がある。",
-            duration:6,
-            name:"Spring",
-            self_evaluation:2,
-            term:"serverside"
-        },
-        {
-            category:{
-                id:2,
-                name:"フレームワーク"
-            },
-            created_at:"2019-06-23T06:27:35.065Z",
-            detail:"基本的なCRUDを作成することができる。",
-            duration:3,
-            name:"rubyonRails",
-            self_evaluation:2,
-            term:"serverside"
-        }
-    ];
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios(
+                'http://localhost:1999/skills?term=frontend',
+            );
+            setData(result.data);
+        };
 
-    const list = skills.map((item) => {
+        fetchData();
+    },[]);
+
+
+    const list = data.skills.map((s: Skill) => {
         return (
             <Grid item xs={12} md={6}>
                 <CardActionArea component="a" href="#">
@@ -113,13 +54,13 @@ const Skills: React.FC = () => {
                         <div className={classes.cardDetails}>
                             <CardContent>
                                 <Typography component="h2">
-                                    {item.name}
+                                    {s.name}
                                 </Typography>
                                 <Typography variant="subtitle1" color="textSecondary">
-                                    {item.term}
+                                    {s.term}
                                 </Typography>
                                 <Typography variant="subtitle1" paragraph>
-                                    {item.detail}
+                                    {s.detail}
                                 </Typography>
                                 <Typography variant="subtitle1" color="primary">
                                     Continue reading...
@@ -136,9 +77,12 @@ const Skills: React.FC = () => {
     })
 
     return (
-        <Grid container spacing={4} >
-            {list}
-        </Grid>
+        <div>
+            <h1>This is Skills!!</h1>
+            <Grid container spacing={4} >
+                {list}
+            </Grid>
+        </div>
     );
 
 }
