ed045c5296f520c635104a6fedfd07095046a717
azul915 <k.aoyagi940516@gmail.com>
2020-02-02 06:13:16 +0900

    Modify fetching each term data

--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -24,10 +24,15 @@ const Skills: React.FC<Props> = (props: Props) => {
 
     const classes = useStyles();
 
+    const serverside = props.skills.filter(skill => skill.term === 'serverside');
+    const frontend = props.skills.filter(skill => skill.term === 'frontend');
+    const infrastructure = props.skills.filter(skill => skill.term === 'infrastructure');
+
     return (
         <div>
             <Grid container spacing={4} >
-                {props.skills.map((skill, index) => (
+
+                {serverside.map((skill, index) => (
                     <Grid item xs={12} md={6} key={index}>
                         <CardActionArea component="a" href="#">
                             <Card className={classes.card}>
@@ -51,6 +56,57 @@ const Skills: React.FC<Props> = (props: Props) => {
                         </CardActionArea>
                     </Grid>
                 ))}
+
+                {frontend.map((skill, index) => (
+                    <Grid item xs={12} md={6} key={index}>
+                        <CardActionArea component="a" href="#">
+                            <Card className={classes.card}>
+                                <div className={classes.cardDetails}>
+                                    <CardContent>
+                                        <Typography component="h2">
+                                            {skill.name}
+                                        </Typography>
+                                        <Typography variant="subtitle1" color="textSecondary">
+                                            {skill.term}
+                                        </Typography>
+                                        <Typography variant="subtitle1" paragraph>
+                                            {skill.detail}
+                                        </Typography>
+                                        <Typography variant="subtitle1" color="primary">
+                                            Continue reading...
+                                        </Typography>
+                                    </CardContent>
+                                </div>
+                            </Card>
+                        </CardActionArea>
+                    </Grid>
+                ))}
+
+                {infrastructure.map((skill, index) => (
+                    <Grid item xs={12} md={6} key={index}>
+                        <CardActionArea component="a" href="#">
+                            <Card className={classes.card}>
+                                <div className={classes.cardDetails}>
+                                    <CardContent>
+                                        <Typography component="h2">
+                                            {skill.name}
+                                        </Typography>
+                                        <Typography variant="subtitle1" color="textSecondary">
+                                            {skill.term}
+                                        </Typography>
+                                        <Typography variant="subtitle1" paragraph>
+                                            {skill.detail}
+                                        </Typography>
+                                        <Typography variant="subtitle1" color="primary">
+                                            Continue reading...
+                                        </Typography>
+                                    </CardContent>
+                                </div>
+                            </Card>
+                        </CardActionArea>
+                    </Grid>
+                ))}
+
             </Grid>
         </div>
     );
--- src/app/pages/index.tsx
+++ src/app/pages/index.tsx
@@ -49,8 +49,16 @@ const IndexPage: NextPage<Props> = (props: Props) => {
 
 IndexPage.getInitialProps = async () => {
 
-    const res = await fetch('http://192.168.100.113:1999/skills?term=serverside');
-    const data = await res.json();
+    const serverside_res = await fetch('http://192.168.100.113:1999/skills?term=serverside');
+    const serverside_data = await serverside_res.json();
+
+    const frontend_res = await fetch('http://192.168.100.113:1999/skills?term=frontend');
+    const frontend_data = await frontend_res.json();
+
+    const infrastructure_res = await fetch('http://192.168.100.113:1999/skills?term=infrastructure');
+    const infrastructure_data = await infrastructure_res.json();
+
+    const data = [].concat(serverside_data).concat(frontend_data).concat(infrastructure_data);
     return { skills: data }
 
 };
