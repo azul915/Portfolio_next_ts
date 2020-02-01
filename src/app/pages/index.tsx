d513bf7943795cded067aba1d6fec2e9a647eb7c
azul915 <k.aoyagi940516@gmail.com>
2020-02-01 16:52:54 +0900

    Fetch data in index page

--- src/app/pages/index.tsx
+++ src/app/pages/index.tsx
@@ -1,41 +1,68 @@
 import * as React from 'react'
-
-import Grid from '@material-ui/core/Grid'
-import Layout from '../components/Layout'
 import { NextPage } from 'next'
 
+import Grid from '@material-ui/core/Grid'
+
+import fetch from 'isomorphic-unfetch'
+
+import Layout from '../components/Layout'
 import MainImg from '../components/MainImg'
 import Profile from '../components/Profile'
-import Skills from '../components/Skills'
 import Products from '../components/Products'
 import Blog from '../components/Blog'
 
-const IndexPage: NextPage = () => {
-    return (
-        <Layout title="">
-            <Grid container>
-                <Grid item xs={12}>
-                    <MainImg/>
-                </Grid>
+interface Props {
+    skills: Array<{
+        category: Array<{
+            id: number
+            name: string
+        }>
+        created_at: string
+        detail: string
+        duration: number
+        name: string
+        self_evaluation: number
+        term: string
+    }>
+}
 
-                <Grid item xs={12}>
-                    <Profile/>
-                </Grid>
-
-                <Grid item xs={12}>
-                    <Skills/>
-                </Grid>
-
-                <Grid item xs={12}>
-                    <Products/>
-                </Grid>
-
-                <Grid item xs={12}>
-                    <Blog/>
-                </Grid>
+const IndexPage: NextPage<Props> = (props: Props) => (
+    <Layout title="">
+        <Grid container>
+            <Grid item xs={12}>
+                <MainImg />
             </Grid>
-        </Layout>
-    )
+
+            <Grid item xs={12}>
+                <Profile />
+            </Grid>
+
+            <Grid item xs={12}>
+                <ul>
+                    {props.skills.map((skill, index) => (
+                        <li key={index}>
+                            <a>{skill.name}</a>
+                        </li>
+                    ))}
+                </ul>
+            </Grid>
+
+            <Grid item xs={12}>
+                <Products />
+            </Grid>
+
+            <Grid item xs={12}>
+                <Blog />
+            </Grid>
+        </Grid>
+    </Layout>
+);
+
+IndexPage.getInitialProps = async () => {
+
+    const res = await fetch('http://192.168.100.113:1999/skills?term=serverside');
+    const data = await res.json();
+    return { skills: data }
 }
 
 export default IndexPage
