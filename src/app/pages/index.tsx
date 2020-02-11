4869d00320f3da20e6880587f279f349b101b179
azul915 <k.aoyagi940516@gmail.com>
2020-02-11 18:07:29 +0900

    Update getInitialProps

--- src/app/pages/index.tsx
+++ src/app/pages/index.tsx
@@ -48,21 +48,14 @@ const IndexPage: NextPage<Props> = (props: Props) => {
 
 IndexPage.getInitialProps = async () => {
 
-    const serverside_res = await fetch('http://192.168.100.113:1999/skills?term=serverside');
-    const serverside_data = await serverside_res.json();
-
-    const frontend_res = await fetch('http://192.168.100.113:1999/skills?term=frontend');
-    const frontend_data = await frontend_res.json();
-
-    const infrastructure_res = await fetch('http://192.168.100.113:1999/skills?term=infrastructure');
-    const infrastructure_data = await infrastructure_res.json();
+    const skills = await fetch('http://192.168.100.113:1999/skills');
+    const skills_data = await skills.json();
 
     const qiita = await fetch('https://qiita.com/api/v2/users/azul915/items');
     const qiita_data = await qiita.json();
 
-    const data = [].concat(serverside_data).concat(frontend_data).concat(infrastructure_data);
     return { 
-        skills: data,
+        skills: skills_data,
         posts: qiita_data
     }
 
